c48329859755c0f9fd1eb409a8eb61350f0693b0
Jonyi19 <rionerumessi624@gmail.com>
2016-05-08 00:41:59 +0900

    背景画像を設定

--- ShootRecords/ViewController.swift
+++ ShootRecords/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        initImageView()
+    }
+    
+    /**
+     背景画像をセットする
+ UIColorにpatternImageメソッドを使い背景画像を渡して対象となるviewのbackgroundColorにセットするだけ。これで背景画像が表示出来ます。
+ */
+    
+    func initImageView(){
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        UIImage(named: "coat1.png")?.drawInRect(self.view.bounds)
+        
+        let image: UIImage! = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        
+        self.view.backgroundColor = UIColor(patternImage: image)
+    
+    
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
