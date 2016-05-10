47680ddf1901ccf0b42d13632c3683a68bfd9a66
Jonyi19 <rionerumessi624@gmail.com>
2016-05-10 23:59:54 +0900

    Realmを追加

---
+++ ShootRecords/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  ShootRecords
+//
+//  Created by kinjo Ryuta on 2016/05/10.
+//  Copyright © 2016年 jonny. All rights reserved.
+//
+
+//import Foundation
+import RealmSwift
+
+class User: Object{
+    dynamic var id = 1
+    dynamic var name = ""
+}
--- ShootRecords/ViewController.swift
+++ ShootRecords/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
     
@@ -14,6 +15,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         initImageView()
+        
+        let me = User()
+        me.name = "たなか"
+        
+        let realm = try! Realm()
+        try! realm.write{
+            realm.add(me)
+        }
+        print(realm.objects(User))
     }
     
     /**
