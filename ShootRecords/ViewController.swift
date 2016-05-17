fd66c5231f10ecc3592f40a76b211aa22e39a601
Jonyi19 <rionerumessi624@gmail.com>
2016-05-17 23:03:35 +0900

    タッチすると回数が表示され、ボタンを押すと切り替えが出来る。

--- ShootRecords/ViewController.swift
+++ ShootRecords/ViewController.swift
@@ -22,17 +22,6 @@ class ViewController: UIViewController {
         //touch画面が出来る
         cort.userInteractionEnabled = true
         cort.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.imageTapped(_:))))
-//        cort.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "imageTapped:"))
-//        //        Games()
-//
-//        let me = User()
-//        me.name = "たなか"
-//        
-//        let realm = try! Realm()
-//        try! realm.write{
-//            realm.add(me)
-//        }
-//        print(realm.objects(User))
     }
     
     /**
@@ -53,31 +42,72 @@ class ViewController: UIViewController {
         
     
     }
-    @IBAction func twoPoint(sender: AnyObject) {
-         shoot_db.two_point += 1
-    }
-    @IBAction func noPoint(sender: AnyObject) {
-        shoot_db.miss_point += 1
-    }
 
-    //画面をタップすると点が入る。
-    func imageTapped(sender: UITapGestureRecognizer){
-        
-        do{
+    @IBOutlet weak var twoView: UILabel!
+    @IBOutlet weak var noView: UILabel!
+    
+    //０点ボタンの動き
+    @IBAction func noBtn(sender: AnyObject) {
+        print("０点ボタンが押されました")
+        upDateNoCount()
+    }
+    
+    //２点ボタンの動き
+    @IBAction func twoBtn(sender: AnyObject) {
+        print("２点ボタンが押されました")
+        upDateTwoCount()
+    }
+    var shoot = 2
+    
+    //Label２点の切り替え
+    func upDateTwoCount(){
+        shoot = 2
+        twoShoot()
+        let str = String(shoot_db.two_point)
+        self.twoView.text = str
+        return
+    }
+    
+    //Label０点の切り替え
+    func upDateNoCount(){
+        shoot = 0
+        noShoot()
+        let str1 = String(shoot_db.miss_point)
+        self.noView.text = str1
+        return
+    }
+    
+    //２点のカウント
+    func twoShoot(){
         let realm = try! Realm()
         try! realm.write{
-            twoPoint(shoot_db)
-            //            shoot_db.two_point += 1
-            noPoint(shoot_db)
+            shoot_db.two_point += 1
         }
         
         print("２点です \(shoot_db.two_point)")
-            return
+    }
+
+    //０点のカウント
+    func noShoot(){
+        let realm = try! Realm()
+        try! realm.write{
+            shoot_db.miss_point += 1
         }
-        catch{
+        
+        print("0点です \(shoot_db.miss_point)")
+    }
+
     
-        print("テスト");
+    //画面をタップすると点が入る。
+    func imageTapped(sender: UITapGestureRecognizer){
+        
+        if shoot == 2 {
+            self.upDateTwoCount()
+        }else if shoot == 0{
+        self.upDateNoCount()
         }
+            return
+        
     }
     
     
